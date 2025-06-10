4ac65397a732a5994bf5a72346dde9f4d4e6dfa1
shreyashsinha-cs22 <shreyashsinha0017@gmail.com>
2025-06-10 20:10:24 +0530

    initial

---
+++ area.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class area{
+public static void main(String... arg){
+Scanner sc=new Scanner(System.in);
+double radius=sc.nextInt();
+double area = 3.14 * Math.pow(radius,2);
+System.out.print(area);
+}
+}
