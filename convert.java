982c6265f29279373ce0f96a9e2a879d8b27726f
shreyashsinha-cs22 <shreyashsinha0017@gmail.com>
2025-06-10 22:43:37 +0530

    initial

---
+++ convert.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class convert{
+public static void main(String... arg){
+Scanner sc=new Scanner(System.in);
+int kilometer=sc.nextInt();
+System.out.println(kilometer*.621371);
+}
+}
