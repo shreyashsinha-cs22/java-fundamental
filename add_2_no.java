0a60ec4c335eb78a54953916defa0029e7506848
shreyashsinha-cs22 <shreyashsinha0017@gmail.com>
2025-06-10 11:16:37 +0530

    initial

---
+++ add_2_no.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class add_2_no{
+public static void main(String... arg){
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	System.out.print(a+b);
+}
+}
